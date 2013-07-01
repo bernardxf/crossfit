bf8d59be19af5ce1733231fd60b711af40f6a3ac
jlmachado1 <joaolmsb@gmail.com>
2013-07-01 13:20:46 -0300

    controller da tela de aluno

--- js/controllers.js
+++ js/controllers.js
@@ -136,14 +136,39 @@ myApp.controller('loginController', function($scope){
 });
 
 myApp.controller('alunoController', function($scope){
-	$scope.pesquisaForm = {},
-	$scope.pesquisa = function(className){
-		var dataset = $scope.pesquisaForm[className];
-		$scope.callMethod(className, 'pesquisa', dataset, function(response){
-			$scope.dataset[className] = JSON.parse(response);
+	$scope.pesquisaForm = {};
+	$scope.pesquisaDataset = {};
+	$scope.form = {};
+	$scope.pesquisa = function(){
+		var dataset = $scope.pesquisaForm;
+		$scope.callMethod('aluno', 'pesquisa', dataset, function(response){
+			$scope.pesquisaDataset = JSON.parse(response);
 			$scope.$apply();
+		});		
+	};
+	$scope.save = function(){
+		$scope.$parent.save('aluno', $scope.form, function(){
+			$scope.pesquisa();
 		});
-	}
+		
+	};
+	$scope.newRow = function(){
+		var dataset = {'_STATE':'I'};
+		$scope.form = dataset;
+		$scope.toggle('form');
+	};
+	$scope.edit = function(index){
+		var dataset = angular.copy($scope.pesquisaDataset[index]);
+		dataset['_STATE'] = 'U';
+		$scope.form = dataset;
+		$scope.toggle('form');
+	};
+	$scope.deleteRow = function(index){
+		var dataset = $scope.pesquisaDataset[index];
+		$scope.$parent.deleteRow('aluno', dataset, function(){
+			$scope.pesquisa();
+		});
+	};
 });
 
 myApp.controller('presencaController', function($scope){
