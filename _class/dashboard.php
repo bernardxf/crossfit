b195131c6c9870eaeb99dd8d33e837518688c631
jlmachado1 <joaolmsb@gmail.com>
2013-08-09 13:25:37 -0300

    pequeno ajuste no dashboard

--- _class/dashboard.php
+++ _class/dashboard.php
@@ -5,7 +5,7 @@
 	$dataset = $_POST['dataset'];
 
 	if($methodToCall == 'loadData'){
-		$SQL = "SELECT nome, DATE_FORMAT(data_nasc, '%d/%m/%Y') as data_nasc FROM aluno WHERE DATE_FORMAT(data_nasc, '%m') = DATE_FORMAT(SYSDATE(), '%m')";
+		$SQL = "SELECT nome, DATE_FORMAT(data_nasc, '%d/%m/%Y') as data_nasc FROM aluno WHERE DATE_FORMAT(data_nasc, '%m') = DATE_FORMAT(SYSDATE(), '%m') ORDER BY data_nasc";
 	    $aniversarios = DB::get_rows(DB::query($SQL));
 
 	    $result = ['aniversarios' => $aniversarios];
