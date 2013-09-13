67025198d0b03599ed60064b49756639fda153fb
bernardxf <bernardxf@gmail.com>
2013-09-13 10:27:25 -0300

    Inserção dos planosque estão vencendo no página de dashboard. Obs: Não vai precisar fazer dos planos de estacionamento fixes #29

--- _class/dashboard.php
+++ _class/dashboard.php
@@ -8,7 +8,9 @@
 		$SQL = "SELECT nome, DATE_FORMAT(data_nasc, '%d/%m/%Y') as data_nasc FROM aluno WHERE DATE_FORMAT(data_nasc, '%m') = DATE_FORMAT(SYSDATE(), '%m') ORDER BY data_nasc";
 	    $aniversarios = DB::get_rows(DB::query($SQL));
 
-	    $result = array('aniversarios' => $aniversarios);
+	    $plano = DB::get_rows(DB::query("SELECT nome, DATE_FORMAT(plano_fim, '%d/%m/%Y') as plano_fim FROM aluno WHERE aluno_status = 'ativo' AND to_days(plano_fim) - to_days(NOW()) <= 7;"));
+
+	    $result = array('aniversarios' => $aniversarios, 'plano' => $plano);
 	    echo json_encode($result);
 	}
 ?>
